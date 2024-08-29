e84358ff411356b8d31551ab3f9570a28f62dd0b
Elanngo-Madheswaran <114866265+Elanngo-Madheswaran@users.noreply.github.com>
2024-08-29 18:57:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { Document, Page, Text, View, StyleSheet, PDFDownloadLink } from '@react-
 
 const styles = StyleSheet.create({
     page: {
-        padding: 25,
+        padding: 20,
     },
     table: {
         display: 'table',
